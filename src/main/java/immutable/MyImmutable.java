0a4afb1c8323a17a6b7d4532d3d39d35b4ab49c7
linuxea <linuxea@foxmail.com>
2017-08-21 10:33:40 +0800

    创建一个简单的不可变类

---
+++ src/main/java/immutable/MyImmutable.java
@@ -0,0 +1,43 @@
+package immutable;
+
+import java.util.Arrays;
+
+/**
+ * Created by Linuxea on 2017/8/21.
+ * 不可变类
+ */
+public class MyImmutable {
+
+
+	public static void main(String[] argc){
+		char[] values = {'a','b','c','d'};
+		FakeString fakeString = new FakeString(values);
+		System.out.println(fakeString);
+		FakeString fakeString1 = fakeString.get();
+		System.out.println(fakeString == fakeString1); // must be false
+	}
+
+}
+
+final class FakeString implements Cloneable{
+	private final char[] values;
+
+	public FakeString(char[] values) {
+		this.values = values;
+	}
+
+	/**
+	 * return a copy
+	 * @return
+	 */
+	public FakeString get(){
+		return new FakeString(this.values);
+	}
+
+	@Override
+	public String toString() {
+		return "FakeString{" +
+				"values=" + Arrays.toString(values) +
+				'}';
+	}
+}
