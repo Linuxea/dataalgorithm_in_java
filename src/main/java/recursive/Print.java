ce3391ef76064c8a46348ea167723b346ae186b6
linuxea <linuxea@foxmail.com>
2017-10-07 05:46:20 +0800

    recursive

---
+++ src/main/java/recursive/Print.java
@@ -0,0 +1,26 @@
+package recursive;
+
+import java.util.Scanner;
+
+/**
+ * use recursive
+ * Created by Linuxea on 17-10-7.
+ */
+public class Print {
+
+    private Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        Print print = new Print();
+        print.print();
+    }
+
+    public void print() {
+        String line = scanner.nextLine();
+        if (!"exit".equalsIgnoreCase(line)) {
+            print();
+        }
+        System.out.println(line);
+    }
+
+}
